3897de198347883acad629e2302511c7d7fc6a4a
sai <kuppalasaivikram@gmail.com>
2024-08-01 15:01:58 +0530

    load balancer listener, target group added to control via public IP

--- load-balancers/main.tf
+++ load-balancers/main.tf
@@ -31,3 +31,26 @@ resource "aws_alb" "main" {
   }
 
 }
+resource "aws_lb_target_group" "main" {
+  name     = "${var.name}-${var.env}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "main" {
+  target_group_arn = aws_lb_target_group.main.arn
+  target_id        = var.instance_id
+  port             = 80
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_alb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
